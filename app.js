564a4bb101df3bf526fea8a24eca080d2b737504
Tia Beiruty <tiabeiruty@tias-macbook-air.home>
2023-03-25 23:25:49 +0300

    Completed Code

--- app.js
+++ app.js
@@ -1,1 +1,63 @@
+//ADD LABEL IN HTML
+
+
+const bookItself = document.querySelector('#input-book')
+const bookAuthor = document.querySelector('#input-author')
+const bookButton = document.querySelector('#button-book')
+let cache = {}
+// const authorButton = document.querySelector('#button-author')
+const theTable = document.querySelector('#tb')
+const row = theTable.insertRow()
+const form = document.querySelector('#book-name')
+
+function onAddInformation(e) {
+    e.preventDefault()
+    for (let keys in cache) {
+        
+        if (bookAuthor.value === keys && bookItself.value === cache[keys]) {
+            alert(`${bookItself.value} by ${bookAuthor.value} already exists!`)  
+            return
+        } 
+        
+    }
+        
+        theTable.innerHTML += `
+            <tr>
+                <td>${bookItself.value}</td>
+                <td>${bookAuthor.value}</td>
+                <td><button class="delete-btn">Delete</button></td>
+            </tr>
+            `
+            cache[bookAuthor.value] = bookItself.value
+    
+    
+}
+console.log(cache)
+function onDelete(e) {
+    const btn = e.target
+    if (btn.classList[0] === 'delete-btn') {
+        btn.closest('tr').remove()
+        delete cache[bookAuthor.value]
+    }
+}
+form.addEventListener('submit', onAddInformation)
+theTable.addEventListener('click', onDelete)
+
+// bookButton.addEventListener('click', addBook)
+// authorButton.addEventListener('click', addAuthor)
+
+// function addBook(event) {
+//     event.preventDefault()
+//     let bookCell = row.insertCell()
+//     bookCell.innerHTML = bookItself.value
+    
+//     // console.log(bookItself.value)
+// }
+// function addAuthor(event) {
+//     event.preventDefault()
+//     let authorCell = row.insertCell()
+//     authorCell.innerHTML = bookAuthor.value
+// }
+
+
 
